a1922d113b5c6df144fbe27a4f9e47b161696a9a
Victor Mesquita <mesquitadev@gmail.com>
2023-10-03 11:31:40 -0300

    Initial

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+  "graphql": {
+    enabled: true,
+    config: {
+      // set this to true if you want to enable the playground in production
+      playgroundAlways: false,
+    },
+  },
+  "apollo-sandbox": {
+    // enables the plugin only in development mode
+    // if you also want to use it in production, set this to true
+    // keep in mind that graphql playground has to be enabled
+    enabled: process.env.NODE_ENV === "production" ? false : true,
+    // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
+  },
+});
